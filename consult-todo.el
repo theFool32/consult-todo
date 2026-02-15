09577d8dd7f1e6920ae2eea4503f63a75cf69842
lijie <lijie.32@outlook.com>
2026-02-15 22:14:22 +0800

    improve: fix todo in project with gemini

--- consult-todo.el
+++ consult-todo.el
@@ -1,32 +1,15 @@
 ;;; consult-todo.el --- Search hl-todo keywords in consult -*- lexical-binding: t -*-
 
 ;; Copyright (C) 2023 liuyinz
-
 ;; Author: liuyinz <liuyinz@gmail.com>
 ;; Maintainer: liuyinz <liuyinz@gmail.com>
 ;; Created: 2021-10-03 03:44:36
-;; Version: 0.4.1
+;; Version: 0.5.0
 ;; Package-Requires: ((emacs "29.1") (consult "0.35") (hl-todo "3.1.2"))
 ;; Homepage: https://github.com/liuyinz/consult-todo
 ;; License: GPL-3.0-or-later
 
-;; This file is not a part of GNU Emacs.
-
-;; This program is free software; you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation, either version 3 of the License, or
-;; (at your option) any later version.
-
-;; This program is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;; GNU General Public License for more details.
-
-;; You should have received a copy of the GNU General Public License
-;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
 ;;; Commentary:
-
 ;; Provide commands `consult-todo' to search, filter, jump to hl-todo keywords.
 
 ;;; Code:
@@ -65,18 +48,15 @@ Only effective on buffers."
   :group 'consult-todo)
 
 (defcustom consult-todo-use-rg (if (executable-find "rg") t nil)
-  "If non-nil, use `rg' to search keywords in directory."
+  "If non-nil, use `rg' to search keywords in directory.
+This automatically respects .gitignore."
   :type 'boolean
   :group 'consult-todo)
 
 (defcustom consult-todo-dir-preview-key nil
-  "Preview trigger keys for `consult-todo-dir' related command.
-Value can be nil, `any', a single key or a list of keys."
+  "Preview trigger keys for `consult-todo-dir' related command."
   :type '(choice (const :tag "Any key" any)
-                 (list :tag "Debounced"
-                       (const :debounce)
-                       (float :tag "Seconds" 0.1)
-                       (const any))
+                 (list :tag "Debounced" (const :debounce) (float :tag "Seconds" 0.1) (const any))
                  (const :tag "No preview" nil)
                  (key :tag "Key")
                  (repeat :tag "List of keys" key))
@@ -106,35 +86,30 @@ Value can be nil, `any', a single key or a list of keys."
                 ((and (stringp group) (not (rassoc group (consult-todo--narrow))))))
           (setq consult-todo--narrow-extend
                 (cons consult-todo-other (consult-todo--narrow)))
-        (user-error
-         "Consult-todo-other: format error or conflicts with consult-todo-narrow")
         (setq consult-todo--narrow-extend nil))))
 
 (defun consult-todo--format (candidates)
   "Return formatted string according to CANDIDATES."
-  (if candidates
-      (mapcar
-       (pcase-lambda (`(,name ,line ,type ,pos ,narrow ,text))
-         (propertize
-          (format (apply #'format "%%-%ds %%-%ds %%-%ds %%s"
-                         (cl-loop for i to 2
-                                  collect (seq-max (mapcar
-                                                    (lambda(x) (length (nth i x)))
-                                                    candidates))))
-                  (propertize name 'face 'consult-file)
-                  (propertize line 'face 'consult-line-number)
-                  ;; WONTFIX don't support regexp keywords face
-                  (propertize type 'face (hl-todo--combine-face
-                                          (cdr (assoc type hl-todo-keyword-faces))))
-                  text)
-          'consult-location (cons pos line)
-          'consult--type narrow))
-       candidates)
-    (user-error "No hl-todo keywords")))
+  (when candidates
+    (mapcar
+     (pcase-lambda (`(,name ,line ,type ,pos ,narrow ,text))
+       (propertize
+        (format (apply #'format "%%-%ds %%-%ds %%-%ds %%s"
+                       (cl-loop for i to 2
+                                collect (seq-max (mapcar
+                                                  (lambda(x) (length (nth i x)))
+                                                  candidates))))
+                (propertize name 'face 'consult-file)
+                (propertize line 'face 'consult-line-number)
+                (propertize type 'face (hl-todo--combine-face
+                                        (cdr (assoc type hl-todo-keyword-faces))))
+                text)
+        'consult-location (cons pos line)
+        'consult--type narrow))
+     candidates)))
 
 (defun consult-todo-grep-state ()
-  "Lookup SELECTED in CANDIDATES list of `consult-location' category.
-Return the location marker."
+  "Lookup SELECTED in CANDIDATES list of `consult-location' category."
   (let ((open (consult--temporary-files))
         (jump (consult--jump-state)))
     (lambda (action cand)
@@ -149,8 +124,7 @@ Return the location marker."
                               (nth 1 cand) (nth 2 cand)))))))
 
 (defun consult-todo--candidates (buffers)
-  "Return list of hl-todo keywords in current buffer.
-If optional argument BUFFERS is non-nil, operate on list of them."
+  "Return list of hl-todo keywords in current buffer."
   (cl-loop for buf in (or buffers (list (current-buffer)))
            append
            (with-current-buffer buf
@@ -162,17 +136,11 @@ If optional argument BUFFERS is non-nil, operate on list of them."
                           when (or (null consult-todo-only-comment)
                                    (nth 4 (syntax-ppss)))
                           collect
-                          ;; HACK when buffer is too large, match-string return nil
-                          ;; in one match, use thing-at-point to get the match-string
-                          ;; as type. At this failed match, point is still accurate,
-                          ;; match-begin and match-end is limit to accessible portion
-                          ;; of buffer
                           (let ((type (or (match-string-no-properties 2)
                                           (save-excursion
                                             (backward-to-word)
                                             (substring-no-properties
-                                             (save-match-data
-                                               (thing-at-point 'word)))))))
+                                             (save-match-data (thing-at-point 'word)))))))
                             (list (buffer-name)
                                   (number-to-string (line-number-at-pos))
                                   type
@@ -180,135 +148,129 @@ If optional argument BUFFERS is non-nil, operate on list of them."
                                   (car (or (rassoc type (consult-todo--narrow))
                                            consult-todo-other))
                                   (string-trim (buffer-substring-no-properties
-                                                (point)
-                                                (line-end-position)))))))))))
+                                                (point) (line-end-position)))))))))))
+
+(defun consult-todo--parse-grep-buffer (buffer)
+  "Parse grep BUFFER content using regex. Strict case sensitive."
+  (with-current-buffer buffer
+    (goto-char (point-min))
+    (let ((candidates '())
+          ;; 强制大小写敏感
+          (case-fold-search nil)
+          ;; 匹配行：File:Line:Content (兼容 Windows 路径)
+          (line-re "^\\(.*\\):\\([0-9]+\\):\\(.*\\)$")
+          ;; 构建匹配关键字的正则，不包含冒号，因为我们要在 content 里找这个词
+          ;; 但在 rgrep 阶段我们已经严格限制了冒号
+          (keywords-re (regexp-opt (mapcar #'car hl-todo-keyword-faces))))
+      (while (re-search-forward line-re nil t)
+        (let* ((file (match-string-no-properties 1))
+               (line (string-to-number (match-string-no-properties 2)))
+               (content (match-string-no-properties 3))
+               (full-path (expand-file-name file compilation-directory)))
+
+          ;; 再次确认内容中包含关键字
+          (when (string-match keywords-re content)
+            (let* ((type (match-string 0 content))
+                   (narrow (car (or (rassoc type (consult-todo--narrow))
+                                    consult-todo-other))))
+              (push (list (file-name-nondirectory file)
+                          (number-to-string line)
+                          type
+                          (list full-path line 0)
+                          narrow
+                          (string-trim content))
+                    candidates)))))
+      (nreverse candidates))))
 
 (defun consult-todo--candidates-rgrep (buffer message)
-  "Return list of hl-todo keywords in directory where rgrep running.
-BUFFER is the rgrep buffer, MESSAGE is the status of rgrep."
-  (consult--forbid-minibuffer)
-  (consult--read
-   (consult-todo--format
-    (unwind-protect
-        (when (and (string-match-p "^finished" message)
-                   (string-prefix-p "*consult-todo-" (buffer-name buffer)))
-          (with-current-buffer buffer
-            (goto-char (point-min))
-            (cl-loop while (and (null (eobp))
-                                (condition-case nil
-                                    (progn
-                                      (compilation-next-error 1)
-                                      t)
-                                  (user-error nil)))
-                     when (save-excursion
-                            (save-match-data
-                              (text-property-search-forward 'font-lock-face 'match t)))
-                     collect
-                     (let* ((msg (get-text-property (point) 'compilation-message))
-                            (loc (compilation--message->loc msg))
-                            (line (compilation--loc->line loc))
-                            (col (compilation--loc->col loc))
-                            (file (caar (compilation--loc->file-struct loc)))
-                            (type (buffer-substring-no-properties
-                                   (prop-match-beginning it)
-                                   (prop-match-end it))))
-                       (list (file-name-nondirectory file)
-                             (number-to-string line)
-                             type
-                             (list (expand-file-name file compilation-directory)
-                                   line col)
-                             (car (or (rassoc type (consult-todo--narrow))
-                                      consult-todo-other))
-                             (string-trim
-                              (buffer-substring-no-properties
-                               (prop-match-end it)
-                               (line-end-position))))))))
-      (kill-buffer buffer)
-      (remove-hook 'compilation-finish-functions #'consult-todo--candidates-rgrep)))
-   :prompt "Go to hl-todo in dir: "
-   :category 'consult-grep
-   :require-match t
-   :sort nil
-   :preview-key consult-todo-dir-preview-key
-   :group (consult--type-group (consult-todo--narrow-extend))
-   :narrow (consult--type-narrow (consult-todo--narrow-extend))
-   :lookup #'consult--lookup-member
-   :state (consult-todo-grep-state)))
+  "Sentinel function for async grep."
+  (if (not (buffer-live-p buffer))
+      (ignore)
+    (consult--forbid-minibuffer)
+    (let ((candidates
+           (unwind-protect
+               (when (and (string-match-p "^finished" message)
+                          (string-prefix-p " *consult-todo-" (buffer-name buffer)))
+                 (consult-todo--parse-grep-buffer buffer))
+             ;; Cleanup
+             (kill-buffer buffer))))
+      (if candidates
+          (consult--read
+           (consult-todo--format candidates)
+           :prompt "Go to hl-todo in dir: "
+           :category 'consult-grep
+           :require-match t
+           :sort nil
+           :preview-key consult-todo-dir-preview-key
+           :group (consult--type-group (consult-todo--narrow-extend))
+           :narrow (consult--type-narrow (consult-todo--narrow-extend))
+           :lookup #'consult--lookup-member
+           :state (consult-todo-grep-state))
+        (message "No hl-todo keywords found.")))))
 
 ;;;###autoload
 (defun consult-todo (&optional buffers)
-  "Jump to hl-todo keywords.
-If BUFFERS is non-nil, prompt with hl-todo keywords in them instead."
+  "Jump to hl-todo keywords."
   (interactive "P")
   (consult--forbid-minibuffer)
-  (consult--read
-   (consult-todo--format
-    (consult-todo--candidates buffers))
-   :prompt "Go to hl-todo: "
-   :category 'consult-location
-   :require-match t
-   :sort nil
-   :group (consult--type-group (consult-todo--narrow-extend))
-   :narrow (consult--type-narrow (consult-todo--narrow-extend))
-   :lookup #'consult--lookup-location
-   :state (consult--jump-state)))
+  (let ((candidates (consult-todo--candidates buffers)))
+    (if candidates
+        (consult--read
+         (consult-todo--format candidates)
+         :prompt "Go to hl-todo: "
+         :category 'consult-location
+         :require-match t
+         :sort nil
+         :group (consult--type-group (consult-todo--narrow-extend))
+         :narrow (consult--type-narrow (consult-todo--narrow-extend))
+         :lookup #'consult--lookup-location
+         :state (consult--jump-state))
+      (message "No hl-todo keywords found."))))
+
+(defun consult-todo--make-rg-regexp ()
+  "Construct regex: Word boundary + Keyword + Colon."
+  (let ((keywords (mapcar #'car hl-todo-keyword-faces)))
+    ;; 核心逻辑：(TODO|FIXME):
+    ;; 1. mapconcat 生成 TODO|FIXME
+    ;; 2. \\b 保证前边界
+    ;; 3. 结尾加 : 保证只匹配带冒号的
+    (concat "\\b(?:" (mapconcat #'regexp-quote keywords "|") "):")))
 
 ;;;###autoload
 (defun consult-todo-dir (&optional directory files)
   "Jump to hl-todo keywords in FILES in DIRECTORY.
-If optinal arg FILES is nil, search in all files.
-If optional arg DIRECTORY is nil, rgrep in default directory."
+Strictly matches 'KEYWORD:' (case-sensitive)."
   (interactive)
-  (let* ((files (or files "* .*"))
-         (directory (or directory default-directory)))
+  (let* ((directory (or directory default-directory)))
+
     (add-hook 'compilation-finish-functions #'consult-todo--candidates-rgrep)
-    (cl-letf ((compilation-buffer-name-function
-               (lambda (&rest _) (format "*consult-todo-%s*" directory))))
-      (save-window-excursion
-        ;;  HACK: use `lgrep' instead `find'
-        (if consult-todo-use-rg
-            (let ((grep-template))
-              (grep-apply-setting 'grep-template "rg --no-heading -H <R> <D>")
-              (lgrep (concat  "\\b" (replace-regexp-in-string "\\\\[<>]*" "" (hl-todo--regexp)) "\\b") files directory))
-          (let ((grep-command "grep --color=auto -nH --null -I -e "))
-            (rgrep (hl-todo--regexp) files directory)))
-        )))
-  )
 
+    ;; 强制屏蔽窗口，使用 cl-letf 将 display-buffer 设为 ignore
+    (cl-letf (((symbol-function 'display-buffer) #'ignore)
+              (compilation-buffer-name-function
+               (lambda (&rest _) (format " *consult-todo-%s*" directory))))
 
-;; (if ((boundp *rg*) and *rg*)
-;;     ;;  HACK: speed up with `ripgrep'
-;;     ;; (grep-apply-setting 'grep-find-template "find <D> <X> -type f <F> -exec rg <C> --no-heading -H  <R> /dev/null {} +")
-;;     (progn
-;;       (grep-apply-setting 'grep-template "rg --no-heading -H <R> <D>")
+      (if (and consult-todo-use-rg (executable-find "rg"))
+          ;; --- Ripgrep 逻辑 ---
+          (let* ((regexp (consult-todo--make-rg-regexp))
+                 ;; 参数说明：
+                 ;; -s: 大小写敏感 (Case Sensitive)
+                 ;; -e regexp: 指定正则
+                 (cmd (format "rg -s --no-heading --line-number --color=never --with-filename -e %s %s"
+                              (shell-quote-argument regexp)
+                              (shell-quote-argument (directory-file-name directory)))))
+            (compilation-start cmd 'grep-mode))
 
-;;       (defun consult-todo-dir (&optional directory files)
-;;         "Jump to hl-todo keywords in FILES in DIRECTORY.
-;; If optinal arg FILES is nil, search in all files.
-;; If optional arg DIRECTORY is nil, rgrep in default directory."
-;;         (interactive)
-;;         ))
-;;   (defun consult-todo-dir (&optional directory files)
-;;     "Jump to hl-todo keywords in FILES in DIRECTORY.
-;; If optinal arg FILES is nil, search in all files.
-;; If optional arg DIRECTORY is nil, rgrep in default directory."
-;;     (interactive)
-;;     (let* ((files (or files "* .*"))
-;;            (directory (or directory default-directory)))
-;;       (add-hook 'compilation-finish-functions #'consult-todo--candidates-rgrep)
-;;       (cl-letf ((compilation-buffer-name-function
-;;                  (lambda (&rest _) (format "*consult-todo-%s*" directory))))
-;;         (save-window-excursion
-;;           ))))
-;;   )
+        ;; --- Fallback 逻辑 (不推荐，不支持 gitignore 和严格冒号匹配) ---
+        (save-window-excursion
+          (let ((files (or files "* .*")))
+            (rgrep (hl-todo--regexp) files directory)))))))
 
 ;;;###autoload
 (defun consult-todo-all ()
   "Jump to hl-todo keywords in all `hl-todo-mode' enabled buffers."
   (interactive)
-  (consult-todo (seq-filter (lambda (x)
-                              (buffer-local-value 'hl-todo-mode x))
-                            (buffer-list))))
+  (consult-todo (seq-filter (lambda (x) (buffer-local-value 'hl-todo-mode x)) (buffer-list))))
 
 ;;;###autoload
 (defun consult-todo-project ()
@@ -319,8 +281,7 @@ If optional arg DIRECTORY is nil, rgrep in default directory."
      (expand-file-name
       (if (fboundp 'project-root)
           (project-root project)
-        (car (with-no-warnings
-               (project-roots project))))))))
+        (car (with-no-warnings (project-roots project))))))))
 
 (provide 'consult-todo)
 ;;; consult-todo.el ends here
